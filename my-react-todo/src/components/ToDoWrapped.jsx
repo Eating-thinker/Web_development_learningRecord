1ab3ec82b0796c336af39bd10c75b280b12cafce
Eating <a0977456056@gmail.com>
2025-11-10 11:43:04 +0800

    update react

--- my-react-todo/src/components/ToDoWrapped.jsx
+++ my-react-todo/src/components/ToDoWrapped.jsx
@@ -4,21 +4,34 @@ import Todo from "./Todo";
 function ToDoWrapper() {
 
   const [todos,setTodos] = useState([
-    {content:'打掃廁所',id:Math.random()},
-    {content:'買菜',id:Math.random()},
+    {content:'打掃廁所',id:Math.random(),isCompleted:false},
+    {content:'買菜',id:Math.random(),isCompleted:false},
   ]);
 
   function addTodo(content){
-    const newTodo={content:content,id:Math.random()};
+    const newTodo={content:content,id:Math.random(),isCompleted:false};
     setTodos([...todos,newTodo]);
   }
 
+  function deleteTodo(id){
+    setTodos(todos.filter(function(todo){
+      return todo.id !== id;
+    }))
+  }
+
+  function toggleComplete(id){
+    setTodos(todos.map(function(todo){
+      return todo.id === id 
+      ? {...todo,isCompleted:!todo.isCompleted} 
+      : todo;
+    }))
+  }
   return (
     <div className="ToDoWrapper">
       <h1>代辦事項</h1>
       <CreateForm addTodo={addTodo}></CreateForm>
       {todos.map(function(todo){
-        return <Todo todo={todo} key={todo.id}></Todo>
+        return <Todo todo={todo} key={todo.id} deleteTodo={deleteTodo} toggleComplete={toggleComplete}></Todo>
       })
       }
     </div>
--- my-react-todo/src/components/Todo.jsx
+++ my-react-todo/src/components/Todo.jsx
@@ -1,6 +1,12 @@
-function Todo({todo}){
-    return<div className="todo" >
-        <p >{todo.content}</p>
+import { MdDelete, MdEdit } from "react-icons/md";
+function Todo({todo,deleteTodo,toggleComplete}){
+    return<div className={`todo ${todo.isCompleted ? 'completed' : ''} `}>
+        <p onClick={function(){toggleComplete(todo.id)}}>{todo.content}</p>
+        <div> 
+            <MdEdit style={{cursor:'pointer'}} />
+            <MdDelete onClick={function(){deleteTodo(todo.id)}} style={{cursor:'pointer',marginLeft:'5px'}} />
+        </div>
+        
     </div>
 }
 export default Todo;
