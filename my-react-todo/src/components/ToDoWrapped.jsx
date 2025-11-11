55fccf194466dbf00b975d5c644dbac8f34d00a0
Eating-thinker <a0977456056@gmail.com>
2025-11-11 12:08:05 +0800

    my_react_todo is completed

--- my-react-todo/src/components/ToDoWrapped.jsx
+++ my-react-todo/src/components/ToDoWrapped.jsx
@@ -4,12 +4,12 @@ import Todo from "./Todo";
 function ToDoWrapper() {
 
   const [todos,setTodos] = useState([
-    {content:'打掃廁所',id:Math.random(),isCompleted:false},
-    {content:'買菜',id:Math.random(),isCompleted:false},
+    {content:'打掃廁所',id:Math.random(),isCompleted:false,isEditing:false},
+    {content:'買菜',id:Math.random(),isCompleted:false,isEditing:false},
   ]);
 
   function addTodo(content){
-    const newTodo={content:content,id:Math.random(),isCompleted:false};
+    const newTodo={content:content,id:Math.random(),isCompleted:false,isEditing:false};
     setTodos([...todos,newTodo]);
   }
 
@@ -26,12 +26,31 @@ function ToDoWrapper() {
       : todo;
     }))
   }
+
+
+  function toggleEditing(id){
+    setTodos(todos.map(function(todo){
+      return todo.id === id 
+      ? {...todo,isEditing:!todo.isEditing} 
+      : todo;
+    }))
+  }
+
+  function editTodo(id,newContent){
+    setTodos(todos.map(function(todo){
+      return todo.id === id 
+      ? {...todo,content:newContent,isEditing:false} 
+      : todo;
+    }))
+  }
+
   return (
     <div className="ToDoWrapper">
       <h1>代辦事項</h1>
       <CreateForm addTodo={addTodo}></CreateForm>
       {todos.map(function(todo){
-        return <Todo todo={todo} key={todo.id} deleteTodo={deleteTodo} toggleComplete={toggleComplete}></Todo>
+        return <Todo todo={todo} key={todo.id} deleteTodo={deleteTodo} toggleComplete={toggleComplete} 
+        toggleEditing={toggleEditing} editTodo={editTodo}></Todo>
       })
       }
     </div>
--- my-react-todo/src/components/Todo.jsx
+++ my-react-todo/src/components/Todo.jsx
@@ -1,9 +1,11 @@
 import { MdDelete, MdEdit } from "react-icons/md";
-function Todo({todo,deleteTodo,toggleComplete}){
-    return<div className={`todo ${todo.isCompleted ? 'completed' : ''} `}>
+import EditForm from "./Editform";
+function Todo({todo,deleteTodo,toggleComplete,toggleEditing ,editTodo}) {
+    return todo.isEditing ? <EditForm todo={todo} editTodo={editTodo}></EditForm>
+    :<div className={`todo ${todo.isCompleted ? 'completed' : ''} `}>
         <p onClick={function(){toggleComplete(todo.id)}}>{todo.content}</p>
         <div> 
-            <MdEdit style={{cursor:'pointer'}} />
+            <MdEdit onClick={function(){toggleEditing(todo.id)}} style={{cursor:'pointer'}} />
             <MdDelete onClick={function(){deleteTodo(todo.id)}} style={{cursor:'pointer',marginLeft:'5px'}} />
         </div>
         
